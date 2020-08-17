417bdb76308ac38f61f5af06ad9db9dc9a1bdc47
Eric Nguyen <Eric.Nguyen@jhuapl.edu>
2020-08-17 18:13:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lifelong Learning Logger (L2Logger)
+
+The Lifelong Learning Logger (L2Logger) is a library to produce logs for metrics calculation.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# Get the directory of this file
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    name='l2logger',
+    version='0.1.1',
+    description='Lifelong learning logger',
+    long_description=(HERE / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+    author='Eric Nguyen',
+    author_email='Eric.Nguyen@jhuapl.edu',
+    license='UNLICENSED',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ]
+)
